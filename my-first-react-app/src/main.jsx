e15514ceb521dea059d14953e5966380b32a7035
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2024-11-14 14:52:08 -0500

    Evaluating react components

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx' //so that we may place (render) it within the DOM.
+import Greeting from './greeting.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( 
+  //We create a root object by invoking createRoot with an element from our index.html.
+  //We invoke the render method which is attached to our root object
   <StrictMode>
-    <App />
+    <Greeting />
   </StrictMode>,
 )

7fed24d51dfe0513a3de2cb07aa8c5bca0421582
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2024-11-18 20:22:06 -0500

    Rendering compents in JSX

--- my-first-react-app/src/Greeting.jsx
+++ my-first-react-app/src/Greeting.jsx
@@ -1,15 +1,39 @@
+function List(props) {
+
+    if (!props.animals) {
+        // Without the animals property. This is often the case when 
+        // you are fetching from an API, since it might take some time 
+        // to actually retrieve the data, it is good practice to show 
+        // an indicator for that.
+        return <div>Loading...</div>;
+    }
+    
+    if (props.animals.length === 0) {
+        return <div>There are no animals in the list!</div>;
+    }
+
+    return (
+      <ul>
+        {props.animals.map((animal) => {
+            // If the result of the startsWith function is true, then it 
+            // returns the second operand, which is the <li> element, and 
+            // renders it. Otherwise, if the condition is false it just gets ignored.
+          return <li key={animal}>{animal}</li>;
+        })}
+      </ul>
+    );
+  }
+
 
 function Greeting(){
+
+    const animals = ["Lion", "Cow", "Snake", "Lizard"];
+
     return (
-        <>
-            <h1>Test title</h1>
-            <svg>
-                <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
-            </svg>
-            <form>
-                <input type="text"/>
-            </form>
-        </>
+        <div>
+        <h1>Animals: </h1>
+        <List animals={animals} />
+        </div>
     );
 }
 

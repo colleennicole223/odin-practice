6691459709a8f60864567e2af29149fb7111a10b
Colleen N. Fulton <cfulton33@gatech.edu>
2025-02-06 17:52:14 -0500

    Passing props to a Button Component

---
+++ my-first-react-app/src/Button.jsx
@@ -0,0 +1,27 @@
+
+
+function Button({ text = "Click Me!", color = "blue", fontSize = 12, handleClick }) {
+    const buttonStyle = {
+      color: color,
+      fontSize: fontSize + "px"
+    };
+  
+    return (
+      <button onClick={handleClick} style={buttonStyle}>
+        {text}
+      </button>
+    );
+  }
+  
+  export default function App() {
+    const handleButtonClick = (url) => {
+      window.location.href = url;
+    };
+  
+    return (
+      <div>
+        <Button handleClick={() => handleButtonClick('www.theodinproject.com')} />
+      </div>
+    );
+  }
+  

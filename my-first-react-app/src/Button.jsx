2e877eb0ccbf00942be9eaf56cb620e9dfe811ba
Colleen N. Fulton <cfulton33@gatech.edu>
2025-02-09 16:45:24 -0500

    Added a hook on button

--- my-first-react-app/src/Button.jsx
+++ my-first-react-app/src/Button.jsx
@@ -1,6 +1,12 @@
 
 
 function Button({ text = "Click Me!", color = "blue", fontSize = 12, handleClick }) {
+    const[backgroundColor, setBackgroundColor] = useState("pink");
+
+    const onButtonClick = color => () => {
+      setBackgroundColor(color);
+    }
+
     const buttonStyle = {
       color: color,
       fontSize: fontSize + "px"
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx' //so that we may place (render) it within the DOM.
 import Greeting from './greeting.jsx'
+import Button from './Button.jsx'
 
 createRoot(document.getElementById('root')).render( 
   //We create a root object by invoking createRoot with an element from our index.html.
   //We invoke the render method which is attached to our root object
   <StrictMode>
     <Greeting />
+    <Button />
   </StrictMode>,
 )
